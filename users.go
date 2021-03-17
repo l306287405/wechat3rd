8fbd4dc30cc7e8a7a466d76900a0d1401d8718d6
Javen <im@linjiawen.com>
2021-03-17 16:40:06 +0800

    解密方法bugfix

--- users.go
+++ users.go
@@ -1,6 +1,10 @@
 package wechat3rd
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"errors"
 	"github.com/l306287405/wechat3rd/core"
 	"net/url"
 )
@@ -31,4 +35,52 @@ func (s *Server) Jscode2session(appId ,jsCode string) (resp *Jscode2sessionResp,
 
 	err=core.GetRequest(u,req,resp)
 	return
+}
+
+// 解密用户信息
+func (s *Server) AESCBCDecrypt(encryptData, key, iv string) (data []byte,err error) {
+	var(
+		encBytes []byte
+		keyBytes []byte
+		ivBytes []byte
+		block cipher.Block
+		mode cipher.BlockMode
+	)
+	encBytes, err = base64.StdEncoding.DecodeString(encryptData)
+	if err!=nil{
+		return
+	}
+	keyBytes, err = base64.StdEncoding.DecodeString(key)
+	if err!=nil{
+		return
+	}
+	ivBytes, err = base64.StdEncoding.DecodeString(iv)
+	if err!=nil{
+		return
+	}
+
+	block, err = aes.NewCipher(keyBytes)
+	if err != nil {
+		return
+	}
+	if len(encBytes) < block.BlockSize() {
+		err=errors.New("ciphertext too short")
+		return
+	}
+	if len(encBytes)%block.BlockSize() != 0 {
+		err=errors.New("ciphertext is not a multiple of the block size")
+		return
+	}
+	mode = cipher.NewCBCDecrypter(block, ivBytes)
+	mode.CryptBlocks(encBytes, encBytes)
+	// 解填充
+	encBytes = pkcs7UnPadding(encBytes)
+	return encBytes, nil
+}
+
+//去除填充
+func pkcs7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
 }
